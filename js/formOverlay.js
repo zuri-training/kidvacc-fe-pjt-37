bedd2ced42c01e792c74ef3cd441f4b0b9a8bea1
yousouph246 <81578670+yousouph246@users.noreply.github.com>
2021-07-08 23:11:12 +0100

    added nav  and media query

--- js/formOverlay.js
+++ js/formOverlay.js
@@ -1,19 +1,31 @@
 let closeOverlay = document.querySelector(".closebtn");
 let loginBTN = document.getElementById("click_login");
+let navLoginBTN = document.getElementById("nav_login");
+
 let loginContainer = document.getElementById("login_container");
 
 let signupBTN = document.getElementById("click_signup");
+let navSignupBTN = document.getElementById("nav_signup");
 let signupContainer = document.querySelector(".signup_container");
 
 loginBTN.addEventListener("click", () => {
   loginContainer.style.display = "flex";
   closeOverlay.style.display = "block"
-})
+});
 closeOverlay.addEventListener("click", () => {
   loginContainer.style.display = "none";
   closeOverlay.style.display = "none";
-})
+});
 // END OF LOGIN OVERLAY
+//NAV LOGIN
+navLoginBTN.addEventListener("click", () => {
+  loginContainer.style.display = "flex";
+  closeOverlay.style.display = "block"
+});
+closeOverlay.addEventListener("click", () => {
+  loginContainer.style.display = "none";
+  closeOverlay.style.display = "none";
+});
 
 
 
@@ -21,9 +33,20 @@ closeOverlay.addEventListener("click", () => {
 signupBTN.addEventListener("click", () => {
   signupContainer.style.display = "flex";
   closeOverlay.style.display = "block"
-})
+});
 closeOverlay.addEventListener("click", () => {
   signupContainer.style.display = "none";
   closeOverlay.style.display = "none";
-})
+});
+
+// NAV SIGNUP OVERLAY
+navSignupBTN .addEventListener("click", () => {
+  signupContainer.style.display = "flex";
+  closeOverlay.style.display = "block"
+});
+closeOverlay.addEventListener("click", () => {
+  signupContainer.style.display = "none";
+  closeOverlay.style.display = "none";
+});
+
 // END OF SIGNUP OVERLAY
