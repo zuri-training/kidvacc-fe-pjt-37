fbc287f19a172375f8899d4b258f0092cc5bcd21
unknown <felistusobieze@gmail.com>
2021-07-09 14:52:07 +0100

    installed the axios package, modified all pages for conflicts

--- js/signup.js
+++ js/signup.js
@@ -1,13 +1,12 @@
 const input = document.querySelectorAll(".inactive");
 const show = document.querySelectorAll(".password_show");
-const pass = document.querySelectorAll(".hide")
-
+const pass = document.querySelectorAll(".hide");
 
 input.forEach(e => {
     e.addEventListener('click', () => {
         e.classList.add("active");
     })
-})
+});
 
 show.forEach(s => {
     s.addEventListener('click', () => {
@@ -19,4 +18,38 @@ show.forEach(s => {
             }
         })
     })
-})
+});
+// let loginOverlay = document.getElementById("login_container");
+
+// Axios HTTP Requests and Responses 
+let signUpButton = document.getElementById("signup_button");
+signUpButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    let url = "https://kidvacc.herokuapp.com/api/child/rest-auth/registration/";
+    let signUpEmail = document.getElementById("signup_email").value;
+    let signUsername = document.getElementById("signup_username").value;
+    let signUpPassword1 = document.getElementById("signup_password1").value;
+    let signUpPassword2 = document.getElementById("signup_password2").value;
+
+    if (signUpPassword1 === signUpPassword2) {
+        const parentInformation = {
+            "username": signUsername,
+            "email": signUpEmail,
+            "password1": signUpPassword1,
+            "password2": signUpPassword2
+        };
+
+        axios.post(url, parentInformation)
+        .then( () => {
+            window.location.href = "index.html"
+            // loginOverlay.style.display = "flex"
+        }, error => {
+            alert(error)
+        })
+    } else{
+        alert("Password Mismatch")
+    }
+    
+    
+
+});
