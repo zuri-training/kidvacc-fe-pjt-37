336c7eab36bbb51f00571adbfdc8273aca522b1b
yousouph246 <81578670+yousouph246@users.noreply.github.com>
2021-07-12 19:55:00 +0100

    removed responsive nav bar and its properties

--- js/formOverlay.js
+++ js/formOverlay.js
@@ -1,11 +1,14 @@
 let closeOverlay = document.querySelector(".closebtn");
 let loginBTN = document.getElementById("click_login");
-let navLoginBTN = document.getElementById("nav_login");
-
+// responsive navLoginBTN
+// let navLoginBTN = document.getElementById("nav_login");
+// end
 let loginContainer = document.getElementById("login_container");
 
 let signupBTN = document.getElementById("click_signup");
-let navSignupBTN = document.getElementById("nav_signup");
+// responsive signupBTN
+// let navSignupBTN = document.getElementById("nav_signup");
+// end
 let signupContainer = document.querySelector(".signup_container");
 
 loginBTN.addEventListener("click", () => {
@@ -17,15 +20,16 @@ closeOverlay.addEventListener("click", () => {
   closeOverlay.style.display = "none";
 });
 // END OF LOGIN OVERLAY
-//NAV LOGIN
-navLoginBTN.addEventListener("click", () => {
-  loginContainer.style.display = "flex";
-  closeOverlay.style.display = "block"
-});
-closeOverlay.addEventListener("click", () => {
-  loginContainer.style.display = "none";
-  closeOverlay.style.display = "none";
-});
+
+//RESPONSIVE NAV LOGIN
+// navLoginBTN.addEventListener("click", () => {
+//   loginContainer.style.display = "flex";
+//   closeOverlay.style.display = "block"
+// });
+// closeOverlay.addEventListener("click", () => {
+//   loginContainer.style.display = "none";
+//   closeOverlay.style.display = "none";
+// });
 
 
 
@@ -39,14 +43,14 @@ closeOverlay.addEventListener("click", () => {
   closeOverlay.style.display = "none";
 });
 
-// NAV SIGNUP OVERLAY
-navSignupBTN .addEventListener("click", () => {
-  signupContainer.style.display = "flex";
-  closeOverlay.style.display = "block"
-});
-closeOverlay.addEventListener("click", () => {
-  signupContainer.style.display = "none";
-  closeOverlay.style.display = "none";
-});
+// RESPONSIVE NAV SIGNUP OVERLAY
+// navSignupBTN .addEventListener("click", () => {
+//   signupContainer.style.display = "flex";
+//   closeOverlay.style.display = "block"
+// });
+// closeOverlay.addEventListener("click", () => {
+//   signupContainer.style.display = "none";
+//   closeOverlay.style.display = "none";
+// });
 
 // END OF SIGNUP OVERLAY
